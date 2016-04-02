18e85119d245872e973837a0ef122d9680205ba0
Zoheb Mujahid <zoheb@roadbetter-lm.corp.yahoo.com>
2016-04-01 17:52:35 -0700

    first checkin

---
+++ src/main/java/com/yahoo/tns/adhoc/MyServlet.java
@@ -0,0 +1,24 @@
+package com.yahoo.tns.adhoc;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by zoheb on 4/1/16.
+ */
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello Servlet Get</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
